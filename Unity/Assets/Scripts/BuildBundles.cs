0c66acdc89d330d39c1f63550e8f4260677328e8
RangeMachine <rangemachine@gmail.com>
2016-10-04 12:11:18 +0300

    Unity project fix

--- Unity/Assets/Scripts/BuildBundles.cs
+++ Unity/Assets/Scripts/BuildBundles.cs
@@ -10,9 +10,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see<http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (c) 2016 RangeMachine
+ * Copyright © 2016 RangeMachine
  */
 
 using System.IO;
